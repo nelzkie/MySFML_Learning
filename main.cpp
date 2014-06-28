8509a6802ca20798bec78f546de567e766c11f4c
nelzkie <bellchan@Macintosh.local>
2014-06-29 01:21:10 +1200

    Recover

---
+++ main.cpp
@@ -0,0 +1,25 @@
+
+
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+// Here is a small helper for you ! Have a look.
+
+#include "Game.h"
+#include <iostream>
+
+
+
+
+
+
+
+int main(int, char const**)
+{
+    Game game;
+    game.run();
+   
+    
+    
+    return EXIT_SUCCESS;
+}
